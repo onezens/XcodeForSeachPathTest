8f194dc730debc6422742ea63b9d671ae198abee
zhen <mail@wackosix.cn>
2016-10-18 17:48:30 +0800

    init project

---
+++ Classes/XcodeForSeachPathTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XcodeForSeachPathTest
+//
+//  Created by wangzhen on 16/10/18.
+//  Copyright © 2016年 onezen.cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Classes/XcodeForSeachPathTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeForSeachPathTest
+//
+//  Created by wangzhen on 16/10/18.
+//  Copyright © 2016年 onezen.cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
