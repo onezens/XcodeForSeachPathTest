a82a7cdf2107cd5a2af2e72c27d06dfc4f439258
wz <leafnet02@qq.com>
2017-09-05 19:00:06 +0800

    sdk demo

--- LibDemo/LibDemo/ViewController.m
+++ LibDemo/LibDemo/ViewController.m
@@ -7,6 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <APAuthV2Info.h>
+#import "APAuthV2Info.h"
+#import <openssl/aes.h>
+#import "aes.h"
+#import "openssl/aes.h"
+
+#import <AlipaySDK/AlipaySDK.h>
 
 @interface ViewController ()
 
---
+++ LibDemo/LibDemo/prefix-header.h
@@ -0,0 +1,14 @@
+//
+//  prefix-header.h
+//  LibDemo
+//
+//  Created by wz on 2017/9/5.
+//  Copyright © 2017年 onezen.cc. All rights reserved.
+///Users/leaf/Documents/GitHub/XcodeForSeachPathTest/LibDemo/LibDemo/prefix-header.h
+
+#ifndef prefix_header_h
+#define prefix_header_h
+
+#import <Foundation/Foundation.h>
+
+#endif /* prefix_header_h */
